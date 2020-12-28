7c5048380702b430cf3f51da5063c0e626bfd29e
gal0wsky <maciej.gawlowski@zse.krakow.pl>
2020-12-28 20:38:39 +0100

    Dodanie pliku main.cpp

--- GRA/GRA/main.cpp
+++ GRA/GRA/main.cpp
@@ -3,48 +3,53 @@
 
 int main()
 {
-sf::RenderWindow window(sf::VideoMode(200,200), "GRA",sf::Style::Fullscreen);
-sf::Event event;
+    sf::RenderWindow window(sf::VideoMode(200,200), "GRA",sf::Style::Fullscreen);
 
-Gracz gracz;
-sf::Vector2f kierunek;
+    Gracz gracz(10.0f, 100.0f);
+    gracz.ustawPozycje(sf::Vector2f(200.0f, 300.0f));
+    sf::Vector2f kierunek;
 
-window.setVerticalSyncEnabled(true);
-window.setFramerateLimit(60);
-while (window.isOpen())
-{
-    while ( window.pollEvent( event ))
+    window.setVerticalSyncEnabled(true);
+    window.setFramerateLimit(60);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while ( window.pollEvent( event ))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
                 window.close();
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-                {
-                    kierunek.x = -1;
-                    keirunek.y = 0;
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-                {
-                    kierunek.x = 1;
-                    keirunek.y = 0;
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-                {
-                    kierunek.x = 0;
-                    keirunek.y = -1;
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-                {
-                    kierunek.x = 0;
-                    keirunek.y = 1;
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
-                    gracz.biegnij();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            {
+                kierunek.x = -1.0f;
+                kierunek.y = 0.0f;
+                gracz.zmienPozycje(kierunek);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            {
+                kierunek.x = 1.0f;
+                kierunek.y = 0.0f;
+                gracz.zmienPozycje(kierunek);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            {
+                kierunek.x = 0.0f;
+                kierunek.y = -1.0f;
+                gracz.zmienPozycje(kierunek);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            {
+                kierunek.x = 0.0f;
+                kierunek.y = 1.0f;
+                gracz.zmienPozycje(kierunek);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+                gracz.biegnij();
         }
 
         window.clear();
-        gracz.aktualizuj(kierunek, okno);
+        gracz.aktualizuj(window);
         window.display();
     }
 
