ab4d39b71fdba8444fb20891fc31dc520d2afa03
gal0wsky <maciej.gawlowski@zse.krakow.pl>
2020-12-30 20:47:24 +0100

    Refaktoryzacja kodu klasy Gracz

--- GRA/GRA/main.cpp
+++ GRA/GRA/main.cpp
@@ -4,11 +4,12 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200,200), "GRA",sf::Style::Fullscreen);
+    sf::Vector2f rozmiarEkranu = (sf::Vector2f) window.getSize();
 
     float predkoscGracza = 10.0f;
 
-    Gracz gracz(predkoscGracza, 100.0f);
-    gracz.ustawPozycje(sf::Vector2f(200.0f, 300.0f));
+    Gracz gracz(100, predkoscGracza, 100.0f);
+    gracz.ustaw(rozmiarEkranu, sf::Vector2f(200.0f, 300.0f));
     sf::Vector2f kierunek;
 
     window.setVerticalSyncEnabled(true);
@@ -27,31 +28,31 @@ int main()
             {
                 kierunek.x = -1.0f;
                 kierunek.y = 0.0f;
-                gracz.zmienPozycje(kierunek);
+                gracz.przesun(rozmiarEkranu, kierunek);
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
             {
                 kierunek.x = 1.0f;
                 kierunek.y = 0.0f;
-                gracz.zmienPozycje(kierunek);
+                gracz.przesun(rozmiarEkranu, kierunek);
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
             {
                 kierunek.x = 0.0f;
                 kierunek.y = -1.0f;
-                gracz.zmienPozycje(kierunek);
+                gracz.przesun(rozmiarEkranu, kierunek);
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
             {
                 kierunek.x = 0.0f;
                 kierunek.y = 1.0f;
-                gracz.zmienPozycje(kierunek);
+                gracz.przesun(rozmiarEkranu, kierunek);
             }
 
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::LShift)
-                gracz.zmienPredkosc(predkoscGracza * 2.0);
+                gracz.poruszanie(predkoscGracza * 2.0);
             if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::LShift)
-                gracz.zmienPredkosc(predkoscGracza);
+                gracz.poruszanie(predkoscGracza);
 
         window.clear();
         gracz.aktualizuj(window);
