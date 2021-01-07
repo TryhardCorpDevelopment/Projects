e436196443aa32929e6e3802fb10f35d9ef21555
Maciej Gawłowski <maciej.gawlowski@zse.krakow.pl>
2021-01-07 19:08:58 +0100

    Dodanie plików Builder.h, GraczBuilder.h i GraczBuilder.cpp

---
+++ GRA/GRA/GraczBuilder.h
@@ -0,0 +1,28 @@
+#ifndef GraczBuilder_h
+#define GraczBuilder_h
+
+#include "Builder.h"
+#include "Gracz.h"
+#include <string>
+
+class GraczBuilder: public Builder
+{
+private:
+    Gracz* gracz;
+
+public:
+    GraczBuilder(std::string plik );
+    ~GraczBuilder();
+    void resetuj();
+    void tekstura(std::string plik);
+    void zycie(int zycieGracza);
+    void predkosc(float predkoscGracza);
+    void stamina(float staminaGracza);
+    void pozycja(sf::Vector2f ekran, sf::Vector2f wspolrzedne);
+    Gracz* build();
+
+private:
+    bool czyMoznaUstawic(sf::Vector2f ekran, sf::Vector2f wspolrzedne);
+};
+
+#endif
